cf8c789b5512c92d87d59c2c6e614726a0d44956
TimofeyKi <47717379+TimofeyKi@users.noreply.github.com>
2019-02-24 17:58:35 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -3,104 +3,120 @@ let snake;
 let meal;
 
 function setup() {
-createCanvas(600,600)
-frameRate(5);
-w = width / 20
-snake = new SnakeClass();
-meal = new FoodClass();
-meal.piclokation();
-}
-function draw() {
-background("#FF4500");
-snake.show();
-snake.crawl();
-snake.teleport();
-meal.show();
-if (snake.EatingFood(meal)) {
+  createCanvas(600, 600)
+  frameRate(5);
+  w = width / 20
+  snake = new SnakeClass();
+  meal = new FoodClass();
   meal.piclokation();
- }
 }
+
+function draw() {
+  background("#FF4500");
+  snake.show();
+  snake.crawl();
+  snake.teleport();
+  meal.show();
+  if (snake.EatingFood(meal)) {
+    meal.piclokation();
+
+  }
+}
+
 function SnakeClass() {
-this.FoodCounter = 0;
-this.x = 0;
-this.y = 0;
-this.speedX = 0;
-this.speedY = 0;
-this.show = function() {
-  this.tail = [];
-fill("#FFD700");
-rect(this.x,this.y,w,w);
-};
-this.crawl = function() {
-  this.x += this.speedX*w;
-  this.y += this.speedY*w;
-  for (let i = 0; i < this.foodCounter - 1 ;i++) {
-    this.tail[i] - tail [i + 1];
-  }
-
-
-//  this.tail[this.FoodCounter - 1].x = this.x;
-//  this.tail[this.FoodCounter - 1].y = this.y;
-
- };
- this.direction = function(dirX, dirY) {
-   this.speedX = dirX;
-   this.speedY = dirY;
-
- };
-
- this.teleport = function () {
-   if (this.x >= width) {
+  this.FoodCounter = 0;
   this.x = 0;
- };
- if (this.x < 0) {
- this.x = width - w;
-};
-if (this.y >= height) {
-this.y = 0;
+  this.y = 0;
+  this.tail = [];
+  this.speedX = 0;
+  this.speedY = 0;
+
+  this.show = function() {
+    fill("#FFD700");
+    rect(this.x, this.y, w, w);
+
+    for (let i = 0; i < this.tail.length; i++) {
+      fill("#FFD700");
+      rect(this.tail[i].x, this.tail[i].y, w, w);
+    }
+  };
+
+  this.crawl = function() {
+    for (let i = 0; i < this.tail.length - 1; i++) {
+      this.tail[i] = this.tail = [i + 1];
+    }
+
+    this.tail[this.foodCounter - 1] = {
+      x: this.x,
+      y: this.y,
+    };
+
+    this.x += this.speedX * w;
+    this.y += this.speedY * w;
+
+    console.log("длинна массива" + this.tail.lensgth);
+  };
+
+  this.direction = function(dirX, dirY) {
+    this.speedX = dirX;
+    this.speedY = dirY;
+  };
+
+  this.teleport = function() {
+    if (this.x >= width) {
+      this.x = 0;
+    };
+    if (this.x < 0) {
+      this.x = width - w;
+    };
+    if (this.y >= height) {
+      this.y = 0;
+    }
+    if (this.y < 0) {
+      this.y = height - w;
+    }
+  };
+
+  this.EatingFood = function(other) {
+    if (this.x === other.x && this.y === other.y) {
+      this.FoodCounter += 1;
+      console.log("Переменная " + this.FoodCounter);
+      return true;
+    }
+    return false;
+  };
 }
-if (this.y < 0) {
-this.y = height - w;
- }
-}
-this.EatingFood = function (other) {
-  if (this.x === other.x && this.y === other.y) {
-    this.FoodCounter += 1;
-    return true;
-  }
-  return false;
-};
-}
+
 function keyPressed() {
-if (keyCode === UP_ARROW) {
-  snake.direction(0, -1);
- }
- if (keyCode === DOWN_ARROW) {
-snake.direction(0, 1);
+  if (keyCode === UP_ARROW) {
+    snake.direction(0, -1);
+  }
+  if (keyCode === DOWN_ARROW) {
+    snake.direction(0, 1);
   }
   if (keyCode === LEFT_ARROW) {
-snake.direction(-1, 0);
-   }
-   if (keyCode === RIGHT_ARROW) {
-snake.direction(1, 0);
-    }
- }
- function FoodClass() {
-this.x = 0;
-this.y = 0;
-let rows =  width / w;
-let cols = height / w;
+    snake.direction(-1, 0);
+  }
+  if (keyCode === RIGHT_ARROW) {
+    snake.direction(1, 0);
+  }
+}
 
-this.piclokation = function() {
-  this.x = w * int(random(cols));
-  this.y = w * int(random(rows));
-};
 
-this.show = function() {
-  fill("255");
-  rect(this.x, this.y, w, w);
+function FoodClass() {
+  this.x = 0;
+  this.y = 0;
+  let rows = width / w;
+  let cols = height / w;
 
-//  for ()
-};
+  this.piclokation = function() {
+    this.x = w * int(random(cols));
+    this.y = w * int(random(rows));
+  };
 
- }
+  this.show = function() {
+    fill("255");
+    rect(this.x, this.y, w, w);
+
+  };
+}
